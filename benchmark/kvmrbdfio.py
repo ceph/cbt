3dcad1cc5436afc4654388854e787fe757d2db6e
Mark Nelson <mark.nelson@inktank.com>
2014-01-29 15:03:41 -0600

    major cleanup and revamp
    Signed-off-by: Mark Nelson <mark.nelson@inktank.com>

--- benchmark/kvmrbdfio.py
+++ benchmark/kvmrbdfio.py
@@ -12,23 +12,34 @@ class KvmRbdFio(Benchmark):
 
     def __init__(self, config):
         super(KvmRbdFio, self).__init__(config)
+        self.tmp_dir = self.cluster.tmp_dir
         self.concurrent_procs = config.get('concurrent_procs', 1)
-        self.total_procs = self.concurrent_procs * len(settings.getnodes('clients'))
+        self.total_procs = self.concurrent_procs * len(settings.getnodes('clients').split(','))
 
         self.time =  str(config.get('time', '300'))
+        self.ramp = str(config.get('ramp', '0'))
         self.iodepth = config.get('iodepth', 16)
+        self.numjobs = config.get('numjobs', 1)
         self.mode = config.get('mode', 'write')
+        self.rwmixread = config.get('rwmixread', 50)
+        self.rwmixwrite = 100 - self.rwmixread
         self.ioengine = config.get('ioengine', 'libaio')
         self.op_size = config.get('op_size', 4194304)
         self.pgs = config.get('pgs', 2048)
-        self.vol_size = config.get('vol_size', 65536)
+        self.vol_size = config.get('vol_size', 65536) * 0.9
         self.rep_size = config.get('rep_size', 1)
         self.rbdadd_mons = config.get('rbdadd_mons')
         self.rbdadd_options = config.get('rbdadd_options')
-        self.client_ra = config.get('client_ra')
+        self.client_ra = config.get('client_ra', '128')
+
         # FIXME there are too many permutations, need to put results in SQLITE3 
         self.run_dir = '%s/kvmrbdfio/osd_ra-%08d/client_ra-%08d/op_size-%08d/concurrent_procs-%03d/iodepth-%03d/%s' % (self.tmp_dir, int(self.osd_ra), int(self.client_ra), int(self.op_size), int(self.total_procs), int(self.iodepth), self.mode)
         self.out_dir = '%s/kvmrbdfio/osd_ra-%08d/client_ra-%08d/op_size-%08d/concurrent_procs-%03d/iodepth-%03d/%s' % (self.archive_dir, int(self.osd_ra), int(self.client_ra), int(self.op_size), int(self.total_procs), int(self.iodepth), self.mode)
+
+        # Make the file names string
+        self.names = ''
+        for i in xrange(self.concurrent_procs):
+            self.names += '--name=/srv/rbdfio-`hostname -s`-0/cbt-kvmrbdfio-%d ' % i
 
     def exists(self):
         if os.path.exists(self.out_dir):
@@ -37,7 +48,7 @@ class KvmRbdFio(Benchmark):
         return False
 
     def initialize(self): 
-        pass
+#        pass
 #        self.cleanup()
 #        super(KvmRbdFio, self).initialize()
 #        common.setup_ceph()
@@ -50,18 +61,22 @@ class KvmRbdFio(Benchmark):
 
 #        common.pdsh(settings.cluster.get('clients'), 'sudo modprobe rbd').communicate()
 #        for i in xrange(self.concurrent_procs):
-        names = ""
-        for i in xrange(self.concurrent_procs):
-            letter = string.ascii_lowercase[i+2]
-#            common.pdsh(settings.cluster.get('clients'), 'sudo rbd create rbdfio/rbdfio-`hostname -s`-%d --size %d' % (i, self.vol_size)).communicate()
-#            common.pdsh(settings.cluster.get('clients'), 'sudo rbd map rbdfio-`hostname -s`-%d  --pool rbdfio --id admin' % i).communicate()
-#            common.pdsh(settings.cluster.get('clients'), 'sudo echo "%s %s rbdfio rbdfio-`hostname -s`-%d" | sudo tee /sys/bus/rbd/add && sudo /sbin/udevadm settle' % (self.rbdadd_mons, self.rbdadd_options, i)).communicate()
-            common.pdsh(settings.getnodes('clients'), 'sudo mkfs.xfs /dev/vd%s' % letter).communicate()
-            common.pdsh(settings.getnodes('clients'), 'sudo mkdir /srv/rbdfio-`hostname -s`-%d' % i).communicate()
-            common.pdsh(settings.getnodes('clients'), 'sudo mount -t xfs -o noatime,inode64 /dev/vd%s /srv/rbdfio-`hostname -s`-%d' %(letter, i)).communicate()
+#        names = ""
+#        for i in xrange(self.concurrent_procs):
+        for i in xrange(1):
+             letter = string.ascii_lowercase[i+1]
+             common.pdsh(settings.getnodes('clients'), 'sudo mkfs.ext4 /dev/vd%s' % letter).communicate()
+             common.pdsh(settings.getnodes('clients'), 'sudo mkdir /srv/rbdfio-`hostname -s`-%d' % i).communicate()
+             common.pdsh(settings.getnodes('clients'), 'sudo mount -t ext4 -o noatime /dev/vd%s /srv/rbdfio-`hostname -s`-%d' %(letter, i)).communicate()
 
         # Create the run directory
         common.make_remote_dir(self.run_dir)
+
+        # populate the fio files
+        print 'Attempting to populating fio files...'
+        pre_cmd = 'sudo fio --rw=write -ioengine=sync --numjobs=%s --bs=4M --size %dM %s > /dev/null' % (self.numjobs, self.vol_size, self.names)
+        common.pdsh(settings.getnodes('clients'), pre_cmd).communicate()
+
 
     def run(self):
         super(KvmRbdFio, self).run()
@@ -74,24 +89,30 @@ class KvmRbdFio(Benchmark):
         monitoring.start(self.run_dir)
 
         time.sleep(5)
-        names = ""
-        for i in xrange(self.concurrent_procs):
-            names += "--name=/srv/rbdfio-`hostname -s`-%d/cbt-kvmrbdfio " % i
+ #       names = ''
+ #       for i in xrange(self.concurrent_procs):
+ #           names += "--name=/srv/rbdfio-`hostname -s`-%d/cbt-kvmrbdfio " % i
+ #           names += '--name=/srv/rbdfio-`hostname -s`-0/cbt-kvmrbdfio-%d ' % i 
         out_file = '%s/output' % self.run_dir
-        pre_cmd = 'sudo fio --rw=read -ioengine=sync --numjobs=1 --bs=4M --runtime=1 --size %dM %s > /dev/null' % (self.vol_size * 9/10, names)
-        fio_cmd = 'sudo fio --rw=%s -ioengine=%s --runtime=%s --numjobs=1 --direct=1 --bs=%dB --iodepth=%d --size %dM %s > %s' %  (self.mode, self.ioengine, self.time, self.op_size, self.iodepth, self.vol_size * 9/10, names, out_file)
-        print 'Attempting to populating fio files...'
-        common.pdsh(settings.getnodes('clients'), pre_cmd).communicate()
+#        pre_cmd = 'sudo fio --rw=write -ioengine=sync --numjobs=%s --bs=4M --size %dM %s > /dev/null' % (self.numjobs, self.vol_size, self.names)
+        fio_cmd = 'sudo fio'
+        fio_cmd += ' --rw=%s' % self.mode
+        if (self.mode == 'readwrite' or self.mode == 'randrw'):
+            fio_cmd += ' --rwmixread=%s --rwmixwrite=%s' % (self.rwmixread, self.rwmixwrite)
+        fio_cmd += ' --ioengine=%s' % self.ioengine
+        fio_cmd += ' --runtime=%s' % self.time
+        fio_cmd += ' --ramp_time=%s' % self.ramp
+        fio_cmd += ' --numjobs=%s' % self.numjobs
+        fio_cmd += ' --direct=1'
+        fio_cmd += ' --bs=%dB' % self.op_size
+        fio_cmd += ' --iodepth=%d' % self.iodepth
+        fio_cmd += ' --size=%dM' % self.vol_size 
+        fio_cmd += ' %s > %s' % (self.names, out_file)
+
         print 'Running rbd fio %s test.' % self.mode
         common.pdsh(settings.getnodes('clients'), fio_cmd).communicate()
-#        ps = []
-#        for i in xrange(self.concurrent_procs):
-#            out_file = '%s/output.%s' % (self.run_dir, i)
-#            p = common.pdsh(settings.cluster.get('clients'), 'sudo fio --rw=%s -ioengine=%s --runtime=%s --name=/srv/rbdfio-`hostname -s`-%d/cbt-rbdfio --numjobs=1 --direct=1 --bs=%dB --iodepth=%d --size %dM > %s' % (self.mode, self.ioengine, self.time, i, self.op_size, self.iodepth, self.vol_size * 9/10, out_file))
-#            ps.append(p)
-#        for p in ps:
-#            p.wait()
         monitoring.stop(self.run_dir)
+
         common.sync_files('%s/*' % self.run_dir, self.out_dir)
 
     def cleanup(self):
@@ -100,7 +121,8 @@ class KvmRbdFio(Benchmark):
 #         common.pdsh(settings.cluster.get('clients'), 'sudo find /dev/rbd* -maxdepth 0 -type b -exec rbd unmap \'{}\' \;').communicate()
 
     def set_client_param(self, param, value):
-         common.pdsh(settings.cluster.get('clients'), 'find /sys/block/vd* ! -iname vda -exec sudo sh -c "echo %s > {}/queue/%s" \;' % (value, param)).communicate()
+         cmd = 'find /sys/block/vd* ! -iname vda -exec sudo sh -c "echo %s > {}/queue/%s" \;' % (value, param)
+         common.pdsh(settings.getnodes('clients'), cmd).communicate()
 
     def __str__(self):
         return "%s\n%s\n%s" % (self.run_dir, self.out_dir, super(KvmRbdFio, self).__str__())
