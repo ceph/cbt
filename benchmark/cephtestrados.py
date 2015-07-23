39b8257d0f3f0af86f92e89bfaa540866f3627c2
Mark Nelson <mnelson@redhat.com>
2015-07-22 19:22:20 -0500

    really get rid of sync in initialize.
    Signed-off-by: Mark Nelson <mnelson@redhat.com>

--- benchmark/cephtestrados.py
+++ benchmark/cephtestrados.py
@@ -63,7 +63,6 @@ class CephTestRados(Benchmark):
     # Initialize may only be called once depending on rebuild_every_test setting
     def initialize(self): 
         super(CephTestRados, self).initialize()
-#        common.sync_files('%s/*' % self.run_dir, self.out_dir)
         return True
 
     def run(self):
