29918a0fcbecb91a002ce566eb94a54a16043acc
Mark Nelson <mnelson@redhat.com>
2015-07-30 08:57:30 -0500

    Should default to not set readahead unless specified rather than 128kb
    Signed-off-by: Mark Nelson <mnelson@redhat.com>

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -11,7 +11,7 @@ class Benchmark(object):
 #        self.cluster = Ceph(settings.cluster)
         self.archive_dir = "%s/%08d/%s" % (settings.cluster.get('archive_dir'), config.get('iteration'), self.getclass())
         self.run_dir = "%s/%08d/%s" % (settings.cluster.get('tmp_dir'), config.get('iteration'), self.getclass())
-        self.osd_ra = config.get('osd_ra', 128)
+        self.osd_ra = config.get('osd_ra', None)
         self.cmd_path = ''
         self.valgrind = config.get('valgrind', None)
         self.cmd_path_full = '' 
@@ -34,7 +34,8 @@ class Benchmark(object):
 
     def run(self):
         print 'Setting OSD Read Ahead to: %s' % self.osd_ra
-        self.cluster.set_osd_param('read_ahead_kb', self.osd_ra)
+        if self.osd_ra:
+            self.cluster.set_osd_param('read_ahead_kb', self.osd_ra)
         print 'Cleaning existing temporary run directory: %s' % self.run_dir
         common.pdsh(settings.getnodes('clients', 'osds', 'mons', 'rgws'), 'sudo rm -rf %s' % self.run_dir).communicate()
         if self.valgrind is not None:
