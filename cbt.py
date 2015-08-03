4bb366a64bf42ac6acc6e391d70f218668622184
koder aka kdanilov <kdanilov@mirantis.com>
2015-08-04 01:54:10 +0300

    cbt.py pythonization, add logging module, replace print in cbt.py with logging

--- cbt.py
+++ cbt.py
@@ -1,56 +1,82 @@
 #!/usr/bin/python
-
+import sys
+import logging
 import argparse
-import yaml
-import os
+import collections
+
 import settings
-import common
 import benchmarkfactory
 from cluster.ceph import Ceph
+from log_support import setup_loggers
 
-def parse_args():
+
+logger = logging.getLogger("cbt")
+
+
+def parse_args(argv):
     parser = argparse.ArgumentParser(description='Continuously run ceph tests.')
     parser.add_argument(
-        '--archive',
-        required = True,
-        help = 'Directory where the results should be archived.',
+        '-a', '--archive',
+        required=True,
+        help='Directory where the results should be archived.',
         )
-
     parser.add_argument(
-        '--conf',
-        required = False,
-        help = 'The ceph.conf file to use.',
+        '-c', '--conf',
+        required=False,
+        help='The ceph.conf file to use.',
         )
     parser.add_argument(
         'config_file',
-        help = 'YAML config file.',
+        help='YAML config file.',
         )
-    args = parser.parse_args()
-    return args
+    return parser.parse_args(argv)
 
-if __name__ == '__main__':
-    ctx = parse_args()
+
+def main(argv):
+    setup_loggers()
+    ctx = parse_args(argv[1:])
     settings.initialize(ctx)
 
-    iteration = 0
-    print settings.cluster
-    global_init = {} 
+    logger.debug("Cluster: {0!r}".format(settings.cluster))
+
+    # koder: ordered dict used to preserve cleanup
+    global_init = collections.OrderedDict()
+
     # FIXME: Create ClusterFactory and parametrically match benchmarks and clusters.
     cluster = Ceph(settings.cluster)
-    while (iteration < settings.cluster.get("iterations", 0)):
-        benchmarks = benchmarkfactory.getAll(cluster, iteration)
-        for b in benchmarks:
-            if b.exists():
-                continue
-            # Tell the benchmark to initialize unless it's in the skip list.
-            if not b.getclass() in global_init:
-                b.initialize()
-                # Skip future initializations unless rebuild requested.
-                if not settings.cluster.get('rebuild_every_test', False):
-                    global_init[b.getclass()] = b
-            b.run()
-            if not b.getclass() in global_init:
+    res_code = 0
+
+    try:
+        for iteration in range(settings.cluster.get("iterations", 0)):
+            benchmarks = benchmarkfactory.getAll(cluster, iteration)
+            for b in benchmarks:
+                if b.exists():
+                    continue
+
+                # Tell the benchmark to initialize unless it's in the skip list.
+                if b.getclass() not in global_init:
+                    b.initialize()
+                    # Skip future initializations unless rebuild requested.
+                    if not settings.cluster.get('rebuild_every_test', False):
+                        global_init[b.getclass()] = b
+
+                b.run()
+
+                if b.getclass() not in global_init:
+                    b.cleanup()
+    except:
+        logger.exception("During tests")
+        res_code = 1
+    finally:
+        for test_cls_name, b in reversed(global_init.items()):
+            try:
                 b.cleanup()
-        iteration += 1
-    for k,b in global_init.items():
-        b.cleanup()
+            except:
+                logger.exception("During cleanup " + test_cls_name)
+                res_code = 1
+
+    return res_code
+
+
+if __name__ == '__main__':
+    exit(main(sys.argv))
