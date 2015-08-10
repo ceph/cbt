df59f6ca4ab642804d362da20cd6fc1e9508f4ec
koder aka kdanilov <kdanilov@mirantis.com>
2015-08-10 15:55:45 +0300

    pythonize common.py, add ut for common.py

--- common.py
+++ common.py
@@ -1,45 +1,49 @@
-import settings
-import subprocess
-import time
-import os
 import errno
 import logging
+import os
+import subprocess
+
+import settings
 
 logger = logging.getLogger("cbt")
 
-def pdsh(nodes, command):
-    args = ['pdsh', '-R', 'ssh', '-w', nodes, command]
-    logger.debug('pdsh: %s' % args)
+
+def popen(args):
+    logger.debug('%s', " ".join(args))
     return subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+
+
+def pdsh(nodes, command):
+    return popen(['pdsh', '-R', 'ssh', '-w', nodes, command])
+
 
 def pdcp(nodes, flags, localfile, remotefile):
-    args = ['pdcp', '-f', '1', '-R', 'ssh', '-w', nodes, localfile, remotefile]
+    args = ['pdcp', '-f', '1', '-R', 'ssh', '-w', nodes]
     if flags:
-        args = ['pdcp', '-f', '1', '-R', 'ssh', '-w', nodes, flags, localfile, remotefile]
-    logger.debug('pdcp: %s', args)
-    return subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+        args += [flags]
+    return popen(args + [localfile, remotefile])
+
 
 def rpdcp(nodes, flags, remotefile, localfile):
-    args = ['rpdcp', '-f', '1', '-R', 'ssh', '-w', nodes, remotefile, localfile]
+    args = ['rpdcp', '-f', '1', '-R', 'ssh', '-w', nodes]
     if flags:
-        args = ['rpdcp', '-f', '1', '-R', 'ssh', '-w', nodes, flags, remotefile, localfile]
-    logger.debug('rpdcp: %s', args)
-    return subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+        args += [flags]
+    return popen(args + [remotefile, localfile])
+
 
 def scp(node, localfile, remotefile):
-    args = ['scp', localfile, '%s:%s' % (node, remotefile)]
-    logger.debug('scp: %s', args)
-    return subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    return popen(['scp', localfile, '%s:%s' % (node, remotefile)])
+
 
 def rscp(node, remotefile, localfile):
-    args = ['scp', '%s:%s' % (node, remotefile), localfile]
-    logger.debug('rscp: %s', args)
-    return subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    return popen(['scp', '%s:%s' % (node, remotefile), localfile])
+
 
 def make_remote_dir(remote_dir):
     logger.info('Making remote directory: %s', remote_dir)
     nodes = settings.getnodes('clients', 'osds', 'mons', 'rgws', 'mds')
     pdsh(nodes, 'mkdir -p -m0755 -- %s' % remote_dir).communicate()
+
 
 def sync_files(remote_dir, local_dir):
     nodes = settings.getnodes('clients', 'osds', 'mons', 'rgws', 'mds')
@@ -47,27 +51,34 @@ def sync_files(remote_dir, local_dir):
     if not os.path.exists(local_dir):
         os.makedirs(local_dir)
 
-    pdsh(nodes, 'sudo chown -R %s.%s %s' % (settings.cluster.get('user'), settings.cluster.get('user'), remote_dir))
+    if 'user' in settings.cluster:
+        pdsh(nodes, 'sudo chown -R {0}.{0} {1}'.format(settings.cluster['user'], remote_dir)).communicate()
     rpdcp(nodes, '-r', remote_dir, local_dir).communicate()
+
 
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
+    except OSError as exc:  # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
-        else: raise
+        else:
+            raise
+
 
 def setup_valgrind(mode, name, tmp_dir):
     valdir = '%s/valgrind' % tmp_dir
     logfile = '%s/%s.log' % (valdir, name)
 
     pdsh(settings.getnodes('clients', 'osds', 'mons', 'rgws', 'mds'), 'mkdir -p -m0755 -- %s' % valdir).communicate()
+
     if mode == 'massif':
         outfile = '%s/%s.massif.out' % (valdir, name)
-        return 'valgrind --tool=massif --soname-synonyms=somalloc=*tcmalloc* --massif-out-file=%s --log-file=%s ' % (outfile, logfile)
+        return ('valgrind --tool=massif --soname-synonyms=somalloc=*tcmalloc* ' +
+                '--massif-out-file=%s --log-file=%s ') % (outfile, logfile)
+
     if mode == 'memcheck':
         return 'valgrind --tool=memcheck --soname-synonyms=somalloc=*tcmalloc* --log-file=%s ' % (logfile)
 
-    logger.debug('valgrind mode: %s is not supported.', mode)
+    logger.warning('valgrind mode: %s is not supported.', mode)
     return ''
