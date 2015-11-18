3b9dcc912099e17dcd1eef55f5cc6608890cfc99
Logan Blyth <logan.blyth@ccur.com>
2015-11-18 11:35:50 -0500

    change order of parameters supplied to rados bench to match the arg parsing

--- benchmark/radosbench.py
+++ benchmark/radosbench.py
@@ -99,7 +99,7 @@ class Radosbench(Benchmark):
                 pool_name = 'rados-bench-`hostname -s`-%s'%i
                 run_name = ''
             rados_bench_cmd = '%s -c %s -p %s bench %s %s %s %s %s --no-cleanup 2> %s > %s' % \
-                 (self.cmd_path_full, self.tmp_conf, pool_name, op_size_str, self.time, mode, concurrent_ops_str, run_name, objecter_log, out_file)
+                 (self.cmd_path_full, self.tmp_conf, pool_name, self.time, mode, op_size_str, concurrent_ops_str, run_name, objecter_log, out_file)
             p = common.pdsh(settings.getnodes('clients'), rados_bench_cmd)
             ps.append(p)
         for p in ps:
