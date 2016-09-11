f1b143188d9c201e7806e9d9fad8d12f2ae0d69f
Ben England <bengland@redhat.com>
2016-09-11 12:16:52 -0400

    move code into base class

--- monitor_collectl.py
+++ monitor_collectl.py
@@ -12,21 +12,14 @@ rawdskfilt = 'cciss/c\d+d\d+ |hd[ab] | sd[a-z]+ |dm-\d+ |xvd[a-z] |fio[a-z]+ | v
 collectl_cmd = 'collectl -s+mYZ -i 1:10 --rawdskfilt "%s" -F0 -f %s'
 
 class monitor_collectl(monitoring.CBTMonitoring):
-    def __init__(self, directory):
-        #super(monitor_collectl, self).__init__(directory)
-        monitoring.CBTMonitoring.__init__(self, directory)
-        self.monitor_threads = None
 
     def start(self):
         monitoring.CBTMonitoring.start(self)
-        self.monitor_threads = common.pdsh(self.nodes, 
-                    collectl_cmd % (rawdskfilt, self.subdirectory), 
-                    continue_if_error=False)
+        self.pdsh_threads.append(
+            common.pdsh(self.nodes, 
+                collectl_cmd % (rawdskfilt, self.subdirectory)))
 
     def stop(self):
         common.pdsh(self.nodes, 'pkill -SIGINT -f collectl').communicate()
-        self.monitor_threads.communicate()
         monitoring.CBTMonitoring.stop(self)
 
-    def postprocess(self, out_dir):
-        monitoring.CBTMonitoring.postprocess(self, out_dir)
