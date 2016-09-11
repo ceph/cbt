96b505be867d00a8e4d451712e10ce8775aa2f37
Ben England <bengland@redhat.com>
2016-09-11 12:26:32 -0400

    move code from benchmarks into their base class

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,6 +1,8 @@
+import os
 import subprocess
 import logging
-
+import monitoring
+import monitoring_factory
 import settings
 import common
 
@@ -26,12 +28,21 @@ class Benchmark(object):
         else:
             self.osd_ra = common.get_osd_ra()
 
+        monitor_list_csv = config.get('run_monitoring_list', '')
+        self.run_monitoring_list = monitoring_factory.factory(
+                                      monitor_list_csv,
+                                      os.path.join(self.run_dir, 'run_monitoring'))
+        self.scrub_monitoring_list = monitoring_factory.factory(
+                                      config.get('scrub_monitoring_list', monitor_list_csv),
+                                      os.path.join(self.run_dir, 'scrub_monitoring'))
+        self.pool_monitoring_list = monitoring_factory.factory(
+                                      config.get('pool_monitoring_list', monitor_list_csv),
+                                      os.path.join(self.run_dir, 'pool_monitoring'))
 
     def getclass(self):
         return self.__class__.__name__
 
     def initialize(self):
-        self.cluster.cleanup()
         use_existing = settings.cluster.get('use_existing', True)
         if not use_existing:
             self.cluster.initialize()
@@ -59,6 +70,21 @@ class Benchmark(object):
 
     def cleanup(self):
         pass
+
+    def do_initial_monitoring(self):
+        if self.scrub_monitoring_list:
+            logger.info('Running scrub monitoring.')
+            monitoring.start(self.scrub_monitoring_list)
+            self.cluster.check_scrub()
+            monitoring.stop(self.scrub_monitoring_list)
+
+    # no need to have every benchmark do this when we can do it all here
+
+    def postprocess_all(self):
+        monitoring.postprocess(self.run_monitoring_list, self.out_dir)
+        if self.config.get('iteration') == 0: # Question: why scrub on iteration 0?
+            for lst in [ self.scrub_monitoring_list, self.pool_monitoring_list ]:
+                monitoring.postprocess(lst, self.out_dir)
 
     def dropcaches(self):
         nodes = settings.getnodes('clients', 'osds') 
