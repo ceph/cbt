e402744716527ed8fd86d2628fb81273b1e3c6a3
noor <noormuhammadmalik95@gmail.com>
2018-10-16 11:20:26 +0500

    doc string for benchmark module

---
+++ benchmark/__init__.py
@@ -0,0 +1,10 @@
+"""This module implements each type of supported benchmark as a file. Including\n
+CephTestRados\n
+Cosbench\n
+GetPut\n
+KvmRbdFio\n
+LibRbdFio\n
+NullBench\n
+RadosBench\n
+RawFio\n
+RbdFio"""
