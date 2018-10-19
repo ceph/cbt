7ac63c6118b799b470e1e4d8e49e6e42d4d87e18
noor <noormuhammadmalik95@gmail.com>
2018-10-19 12:56:17 +0500

    comment monitoring stuff

--- monitoring.py
+++ monitoring.py
@@ -10,28 +10,28 @@ import settings
 def start(directory):
     """Start collectl, perf and blktrace on all nodes in the given directory."""
 
-    # get names of all nodes
-    nodes = settings.getnodes('clients', 'osds', 'mons', 'rgws')
-    # setup the directories to be used by each monitoring tool
-    collectl_dir = '%s/collectl' % directory
-    # perf_dir = '%s/perf' % directory
-    # blktrace_dir = '%s/blktrace' % directory
+#     # get names of all nodes
+#     nodes = settings.getnodes('clients', 'osds', 'mons', 'rgws')
+#     # setup the directories to be used by each monitoring tool
+#     collectl_dir = '%s/collectl' % directory
+#     # perf_dir = '%s/perf' % directory
+#     # blktrace_dir = '%s/blktrace' % directory
 
-    # collectl
-    # a perl regex filter to acquire data of only drive/partition from /proc/diskstats that match
-    # unmatched drive/part data is never collected
-    # + tells it collectl to make this the default filter
-    rawdskfilt = '+cciss/c\d+d\d+ |hd[ab] | sd[a-z]+ |dm-\d+ |xvd[a-z] |fio[a-z]+ | vd[a-z]+ |emcpower[a-z]+ |psv\d+ |nvme[0-9]n[0-9]+p[0-9]+ '
-    # create the collectl directory in the remote hosts
-    common.pdsh(nodes, 'mkdir -p -m0755 -- %s' % collectl_dir)
-    # run collectl remotely with following parameters:
-    # -s -> specify the subsystem to sample data from, by default this is 'cdn' (CPU, disk, network)
-    # +mYZ makes it add subsystems, namely 'm->memory', 'Y->Slabs in detail', 'Z->Processes in detail'
-    # -i -> interval of sampling 1:10 means 1sample-diskstats-etc/second and 1sample-processes/10seconds
-    # --rawdskfilt -> already explained above
-    # -F0 -> flush the output buffers immediately at each data sampling time
-    # -f -> file to use for dumping output data in case of "Record Mode"
-    common.pdsh(nodes, 'collectl -s+mYZ -i 1:10 --rawdskfilt "%s" -F0 -f %s' % (rawdskfilt, collectl_dir))
+#     # collectl
+#     # a perl regex filter to acquire data of only drive/partition from /proc/diskstats that match
+#     # unmatched drive/part data is never collected
+#     # + tells it collectl to make this the default filter
+#     rawdskfilt = '+cciss/c\d+d\d+ |hd[ab] | sd[a-z]+ |dm-\d+ |xvd[a-z] |fio[a-z]+ | vd[a-z]+ |emcpower[a-z]+ |psv\d+ |nvme[0-9]n[0-9]+p[0-9]+ '
+#     # create the collectl directory in the remote hosts
+#     common.pdsh(nodes, 'mkdir -p -m0755 -- %s' % collectl_dir)
+#     # run collectl remotely with following parameters:
+#     # -s -> specify the subsystem to sample data from, by default this is 'cdn' (CPU, disk, network)
+#     # +mYZ makes it add subsystems, namely 'm->memory', 'Y->Slabs in detail', 'Z->Processes in detail'
+#     # -i -> interval of sampling 1:10 means 1sample-diskstats-etc/second and 1sample-processes/10seconds
+#     # --rawdskfilt -> already explained above
+#     # -F0 -> flush the output buffers immediately at each data sampling time
+#     # -f -> file to use for dumping output data in case of "Record Mode"
+#     common.pdsh(nodes, 'collectl -s+mYZ -i 1:10 --rawdskfilt "%s" -F0 -f %s' % (rawdskfilt, collectl_dir))
 
     # perf
     # create the directory to hold all the perf data
@@ -63,23 +63,23 @@ def stop(directory=None):
     """Stops the monitoring stack (collectl, perf, blktrace) on all nodes.
     Additionally 'chown's the blktrace directory to the ceph user if given as argument."""
 
-    # get the names of all nodes
-    nodes = settings.getnodes('clients', 'osds', 'mons', 'rgws')
+#     # get the names of all nodes
+#     nodes = settings.getnodes('clients', 'osds', 'mons', 'rgws')
 
-    # killall kills processes by name, so we're sending 'SIGINT -> keyboard interrupt signal' to all instances of collectl
-    common.pdsh(nodes, 'killall -SIGINT -f collectl').communicate()
-    # same with perf
-    common.pdsh(nodes, 'sudo pkill -SIGINT -f perf_3.6').communicate()
-    # the same with blktrace
-    common.pdsh(settings.getnodes('osds'), 'sudo pkill -SIGINT -f blktrace').communicate()
+#     # killall kills processes by name, so we're sending 'SIGINT -> keyboard interrupt signal' to all instances of collectl
+#     common.pdsh(nodes, 'killall -SIGINT -f collectl').communicate()
+#     # same with perf
+#     common.pdsh(nodes, 'sudo pkill -SIGINT -f perf_3.6').communicate()
+#     # the same with blktrace
+#     common.pdsh(settings.getnodes('osds'), 'sudo pkill -SIGINT -f blktrace').communicate()
     
-    # if a directory was given
-    if directory:
-        # get the cluster handle
-        sc = settings.cluster
-        # chown the blktrace data, so it can be used by 'seekwatcher' to make movies
-        common.pdsh(nodes, 'cd %s/perf;sudo chown %s.%s perf.data' % (directory, sc.get('user'), sc.get('user')))
-        make_movies(directory)
+#     # if a directory was given
+#     if directory:
+#         # get the cluster handle
+#         sc = settings.cluster
+#         # chown the blktrace data, so it can be used by 'seekwatcher' to make movies
+#         common.pdsh(nodes, 'cd %s/perf;sudo chown %s.%s perf.data' % (directory, sc.get('user'), sc.get('user')))
+#         make_movies(directory)
 
 # visualize the blktrace data
 def make_movies(directory):
