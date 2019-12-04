e849f51b1a82abd81788b00ae85cb428a0220571
Mark Nelson <mnelson@redhat.com>
2019-12-04 23:26:41 +0000

    cluster/ceph: Add mount_cmd yaml option.
    Signed-off-by: Mark Nelson <mnelson@redhat.com>

--- client_endpoints/cephfskernel_client_endpoints.py
+++ client_endpoints/cephfskernel_client_endpoints.py
@@ -10,5 +10,5 @@ class CephfsKernelClientEndpoints(CephClientEndpoints):
         self.mount_fs()
 
     def mount_fs_helper(self, node, dir_name):
-        cmd = 'sudo mount -t ceph -o name=admin,secretfile=%s,mds_namespace=%s %s:/ %s' % (self.client_secret, self.name, ','.join(self.mon_addrs), dir_name)
+        cmd = 'sudo %s %s:/ %s -o name=admin,secretfile=%s,mds_namespace=%s' % (self.mount_cmd, ','.join(self.mon_addrs), dir_name, self.client_secret, self.name)
         common.pdsh(node, cmd, continue_if_error=False).communicate()
