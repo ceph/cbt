0203560b7c0cfb130961f91c5d08e23ab4b201a2
Sridhar Seshasayee <sseshasa@redhat.com>
2020-03-10 17:21:02 +0530

    tools: Add scripts to visualize fio output
    Tool to parse Fio workload generation results for interesting statistics
    and generate PyPlot graphs. The idea is for the script to parse both
    json and csv output and create meaningful plots to provide insights into
    ceph performance.
    
    Signed-off-by: Sridhar Seshasayee <sseshasa@redhat.com>

---
+++ tools/fio_visualize_data/fio-plot-stats.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+#
+# fio-plot-stats.py - script to parse fio workload generation results
+# for interesting statistics and generate PyPlot graphs. Idea is to
+# parse results generated in json and csv by fio and make use of PyPlot
+# module to generate graphs that will provide insights on Ceph performance
+#
+# input parameters:
+# The input to this script can be a directory containing fio log file(s)
+# in either,
+# - JSON format
+# - CSV format
+#
+# The input file format option is mandatory. Depending on this, additonal
+# options if preferred may be provided to override the default behavior.
+# The default behavior is to treat each output file in the source
+# directory and generate comparison graphs.
+
+# assumption: All fio files in the source directory having 'json'
+# string in filename are treated as JSON files. Otherwise, the file is
+# assumed to be of type CSV.
+#
+# Example 1:
+# The following command scans the source directory for files having
+# string 'json' in the filenames and scans relevant stats from the files
+# to generate comparison graphs in the destination folder.
+# python3 fio-plot-stats.py -s ~/cbt_logs/json_logs -f json -o write -d ~/cbt_logs/json_logs
+import os
+import argparse
+from fiostatsparser import Parsejson
+from fioplotter import Barplot
+
+def dir_path(path):
+  if os.path.isdir(path):
+    return path
+  else:
+    raise argparse.ArgumentTypeError(f"{path} is not a valid directory.")
+
+def parse_args():
+  parser = argparse.ArgumentParser(description='Generate plots from fio output')
+  parser.add_argument('-f', '--filetype', dest='ftype', required=True, choices=['json', 'csv'], help='type of file to parse')
+  parser.add_argument('-s', '--source', dest='srcdir', required=True, type=dir_path, help='source directory containing fio output files')
+  parser.add_argument('-d', '--destination', dest='destdir', required=False, type=dir_path, help='destination directory to save generated plots')
+  parser.add_argument('-o', '--optype', dest='optype', required=True, choices=['read', 'write'], help='plot read or write stats')
+  args = parser.parse_args()
+
+  return args
+
+if __name__ == '__main__':
+  args = parse_args()
+
+  # Parse data and generate plots
+  if args.ftype == 'json':
+    print("Parsing JSON files...")
+    pj = Parsejson(args)
+    pj.dump_all_stats_in_csv()
+    print("Creating plots...")
+    Barplot(args, pj.get_fio_latdata(), 'lat')
+    Barplot(args, pj.get_fio_pctdata(), 'pct')
+
+  if args.ftype == 'csv':
+    print("Cannot plot CSV data...coming soon")
+
---
+++ tools/fio_visualize_data/fioplotter.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+import json
+import time
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Fioplotter():
+  def __init__(self, ctx):
+    # Initialize basic parameters
+    self.destination = ctx.destdir
+    self.basefile = '/client_stats_plot_'
+    self.baseplotfilename = self.destination + self.basefile
+
+  def get_plot_filename(self):
+    date = time.strftime("%m-%d-%Y", time.localtime())
+    ltime = time.strftime("%I:%M-%S%p", time.localtime())
+    timestamp = date + "_" + ltime
+    plotfilename = self.baseplotfilename + self.stattype + "_" + timestamp + ".pdf"
+    return plotfilename
+
+class Barplot(Fioplotter):
+  def __init__(self, ctx, data, stat):
+    super().__init__(ctx)
+    self.ctx = ctx
+    self.plotdata = data
+    self.stattype = stat
+
+    # Initialize generic plot information
+    self.ylabel = ''
+    self.xlabel = ''
+    self.title = ''
+    self.xticklabels = []
+    self.yticklabels = []
+    self.bar_width = 0.2
+    self.index = 0
+    if stat == 'lat':
+      self.set_avg_clt_lat_metadata()
+    elif stat == 'pct':
+      self.set_pct_clt_lat_metadata()
+    self.generate_bar_plot()
+
+  def set_avg_clt_lat_metadata(self):
+    self.ylabel = 'latency (msec)'
+    self.xlabel = 'latency type'
+    self.title = self.ctx.optype + '-avg client latencies'
+    # TODO: Extract from plot data
+    self.xticklabels = ['slat', 'clat', 'lat']
+    self.index = np.arange(len(self.xticklabels))
+
+  def set_pct_clt_lat_metadata(self):
+    self.ylabel = 'latency (msec)'
+    self.xlabel = 'clat percentiles'
+    self.title = self.ctx.optype + '-clat client percentiles'
+    # TODO: Extract from plot data
+    self.xticklabels = ['95th', '99th', '99.50th', '99.90th', '99.95th', '99.99th']
+    self.index = np.arange(len(self.xticklabels))
+
+  def generate_bar_plot(self):
+    width = 0
+
+    # Create subplot
+    fig, ax = plt.subplots()
+
+    for f in self.plotdata.keys():
+      y = list(self.plotdata[f].values())
+      ax.bar(self.index + width, y, self.bar_width, label=f)
+      width += self.bar_width
+
+    ax.set_ylabel(self.ylabel)
+    ax.set_xlabel(self.xlabel)
+    ax.set_title(self.title)
+    ax.set_xticks(self.index)
+    if len(self.xticklabels):
+      ax.set_xticklabels(self.xticklabels)
+    if len(self.yticklabels):
+      ax.set_yticklabels(self.yticklabels)
+    ax.legend()
+    fig.tight_layout()
+
+    plt.grid(b=True, which='major', color='#666666', linestyle='-', alpha=0.5)
+    plt.minorticks_on()
+    plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+
+    plotfilename = self.get_plot_filename()
+    plt.savefig(plotfilename)
+    print("Created plot: ", plotfilename, "\n")
+
